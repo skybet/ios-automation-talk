a951c3738de68ffb2f5ea4ba332d7bc5ba87c80d
Matthew Glover <matthew.glover@skybettingandgaming.com>
2022-07-22 09:51:05 +0100

    Initial Commit

---
+++ AutomationAnimals/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutomationAnimals
+//
+//  Created by Matthew Glover on 22/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
